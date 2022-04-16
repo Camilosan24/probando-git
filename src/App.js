e901d2c3b9fe8ea683fd259993b1d3d9a08f7007
Camilosan2 <danielcamilo.sanchez@globant.com>
2022-04-16 11:18:47 -0500

    cree mi nuevo proyecto

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esto es una prueba de como usar git
         </p>
         <a
           className="App-link"
